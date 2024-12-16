eec4f00cd5aabe15975b97000883d26e481e0b42
apple <apple@apples-MacBook-Air.local>
2024-12-16 07:39:06 +0100

    resolved

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { kairos } from "viem/chains";
-
+import { liskSepolia } from "viem/chains";
+import {
+  baseSepolia,
+} from "wagmi/chains";
 const config = getDefaultConfig({
   appName: "Alpha",
   projectId: "YOUR_PROJECT_ID",
-  chains: [kairos],
+  chains: [liskSepolia,baseSepolia],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
